39afc0e7560a6582530e69704b0eddcac7ae31a9
Matheus Oliveira <matheus.6148@gmail.com>
2025-12-09 08:40:49 -0300

    feat: get user data

--- src/app/github.ts
+++ src/app/github.ts
@@ -1,8 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Github {
-  
+
+  constructor(private readonly http: HttpClient) {}
+
+  public getUser(username: string): Observable<any> {
+    return this.http.get<any>('https://api.github.com/users/${username}');
+  }
 }
