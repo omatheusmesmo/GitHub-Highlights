b5faae16133e9b4ae5bb694d2d976151268ca507
Matheus Oliveira <matheus.6148@gmail.com>
2025-12-21 16:44:47 -0300

    feat: update routing configuration to include Search and Dashboard components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './dashboard/dashboard';
+import { Search } from './search/search';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Search  },
+  { path: 'dashboard/:username', component: Dashboard}
+];
