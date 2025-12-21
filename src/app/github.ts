8beda87c3e94165446eb33122ad66639f9c037e2
Matheus Oliveira <matheus.6148@gmail.com>
2025-12-21 16:45:04 -0300

    fix: correct string interpolation in getUser and getUserRepos methods

--- src/app/github.ts
+++ src/app/github.ts
@@ -10,10 +10,10 @@ export class Github {
   constructor(private readonly http: HttpClient) {}
 
   public getUser(username: string): Observable<any> {
-    return this.http.get<any>('https://api.github.com/users/${username}');
+    return this.http.get<any>(`https://api.github.com/users/${username}`);
   }
 
   public getUserRepos(username: string): Observable<any>{
-    return this.http.get<any>('https://api.github.com/users/${username}/repos')
+    return this.http.get<any>(`https://api.github.com/users/${username}/repos`);
   }
 }
