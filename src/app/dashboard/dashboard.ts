5b75a647586de04b55f0d06237a66c08fc8e4959
Matheus Oliveira <matheus.6148@gmail.com>
2025-12-21 16:45:17 -0300

    feat: enhance dashboard with user data display and integrate language highlights component

--- src/app/dashboard/dashboard.ts
+++ src/app/dashboard/dashboard.ts
@@ -1,18 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Github } from '../github';
 import { catchError, of } from 'rxjs';
 import { StarCount } from '../star-count/star-count';
+import { LanguageHighlights } from '../language-highlights/language-highlights';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [ StarCount],
+  imports: [ StarCount, LanguageHighlights],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
 export class Dashboard implements OnInit{
 
-  username: string = '';
+  username = signal<string>('');
   userData: any = {};
 
   constructor(
@@ -21,26 +22,29 @@ export class Dashboard implements OnInit{
   ) {}
 
   ngOnInit(): void {
-    this.username = this.route.snapshot.paramMap.get('username') || '';
+    const nameFromUrl = this.route.snapshot.paramMap.get('username') || '';
+    this.username.set(nameFromUrl);
 
     console.log('Dashboard User: ', this.username);
 
-    this.githubService.getUser(this.username)
-      .pipe(
-        catchError(error => {
-          console.error('Error fetching user:', error);
-          return of ({});
-        })
-      )
-      .subscribe({
-        next: (data)=> {
-          console.log('User data:', data);
-          this.userData = data;
-        },
-        error: (err) => {
-          console.error('Subscription error:', err);
-        }
-      });
+    if (this.username()){
+      this.githubService.getUser(this.username())
+        .pipe(
+          catchError(error => {
+            console.error('Error fetching user:', error);
+            return of ({});
+          })
+        )
+        .subscribe({
+          next: (data)=> {
+            console.log('User data:', data);
+            this.userData = data;
+          },
+          error: (err) => {
+            console.error('Subscription error:', err);
+          }
+        });
+    }
   }
 
 }
